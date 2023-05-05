ebd319487df40136bca6e38f67b81cf4b3bd5ee2
garyunwin42 <garyunwin42@gmail.com>
2023-05-05 01:37:21 -0400

    make mock oracle view

--- contracts/mock/MockPriceOracle.sol
+++ contracts/mock/MockPriceOracle.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 contract MockPriceOracle {
     uint256 lastPrice = 1000 * 10 ** 8;
 
-    function getUnderlyingPrice() external returns (uint256) {
+    function getUnderlyingPrice() external view returns (uint256) {
         return lastPrice;
     }
 
